4f98f75388f7559d78a6c73895c4ba4bcd58d7c6
Alexander Lopez <alopez@Alexanders-MacBook-Pro.local>
2022-07-27 12:18:59 -0700

    prettier formatting

--- backend/api/generator.test.js
+++ backend/api/generator.test.js
@@ -1,5 +1,5 @@
-const random_id = require('./generator');
+const random_id = require("./generator");
 
-test("Returns random ID hex string with length of nbytes * 2", () =>  {
-    expect(random_id(50)).toHaveLength(100);
+test("Returns random ID hex string with length of nbytes * 2", () => {
+  expect(random_id(50)).toHaveLength(100);
 });
