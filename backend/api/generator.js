b14b764e1d0772767bb43c42810268f6eeda1348
Alexander Lopez <alopez@Alexanders-MacBook-Pro.local>
2022-08-04 08:12:01 -0700

    rebase

--- backend/api/generator.js
+++ backend/api/generator.js
@@ -14,6 +14,10 @@ let hny = new Libhoney({
   }
 });
 
+hny.sendNow({
+    name: "app.js Initialized",
+    randomFloat: Math.random()
+});
 
 function random_id(nbytes) {
   let hexString = "";
@@ -65,6 +69,7 @@ function endEvent(Event, name, additionalFields = null) {
   }
   Event.metadata = {id: name}
   Event.send();
+  hny.flush();
 }
 
 function clear_maze(trace_id, parent_id = null) {
